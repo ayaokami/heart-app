a9d43c6a23987d1db168cad0c2d52ad0873acf0e
ayaokami <bbaqatb@gmail.com>
2022-02-14 19:12:12 +0900

    コラムモデルの作成
    ジャンルはアクティブハッシュで紐付け

--- db/migrate/20220214094227_create_columns.rb
+++ db/migrate/20220214094227_create_columns.rb
@@ -1,7 +1,9 @@
 class CreateColumns < ActiveRecord::Migration[6.0]
   def change
     create_table :columns do |t|
-
+      t.string   :title     ,null: false
+      t.text     :text      ,null: false
+      t.integer  :genre_id  ,null: false
       t.timestamps
     end
   end
