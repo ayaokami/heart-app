54d9dca892a8db0c20d3aa86e3e4e68b3e31f85f
ayaokami <bbaqatb@gmail.com>
2022-02-14 23:54:28 +0900

    コントローラーの作成

--- app/controllers/admin/columns_controller.rb
+++ app/controllers/admin/columns_controller.rb
@@ -1,2 +1,24 @@
 class Admin::ColumnsController < ApplicationController
+  # 管理者以外はトップページに遷移する
+  before_action :if_not_admin
+
+  # 以下に管理者が行うアクションを記載
+  def new
+    @column = Column.new
+  end
+
+  def creste
+    @column = Column.new(column_params)
+    if @column.valid?
+      @column.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def if_not_admin
+    redirect_to root_path unless current_user.admin?
+  end
 end
