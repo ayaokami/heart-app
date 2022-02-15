c325f2fb430f81b84d95881b15d278e827b1e82d
ayaokami <bbaqatb@gmail.com>
2022-02-15 16:32:58 +0900

    userモデルにバリデーションをかけ、テストコード用のファイルを作成
    ・バリデーションをかけたのでフォームの文字数制限を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :columns
+
+  with_options presence: true do
+    validates :nickname, length: { maximum: 6 }
+    validates :target_weight, length: { maximum: 5 }
+    validates :age, length: { maximum: 3 }
+  end
+  with_options presence: true, format: {with: /\A[ァ-ヶー]+\z/ , message: '全角カタカナを使用してください'} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+    
+    validates :water_restriction, length: { maximum: 4 }
+
 end
