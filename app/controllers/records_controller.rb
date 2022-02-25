ab1dfa8c45dd1127632be3050a106d73ccf7c6bc
ayaokami <bbaqatb@gmail.com>
2022-02-26 01:51:06 +0900

    createアクションの実装
    ・paramsの設定
    ・if分で登録可否を分ける
    ・可否できるようにバリデーション記載
    ・userの紐付けを忘れがち

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,4 +6,19 @@ class RecordsController < ApplicationController
   def new
     @record = Record.new
   end
+
+  def create
+    @record = Record.new(record_params)
+    if @record.valid?
+      @record.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:kt, :pulse, :systolic, :diastolic, :bw, :palpitation, :suffocation, :swelling, :fatigue, :start_time).merge(user_id: current_user.id)
+  end
 end
